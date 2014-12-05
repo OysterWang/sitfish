f98193528ab78ccc30cdaf1b384b36a21af338a1
Long Zhao <buaastorm@gmail.com>
2014-12-06 02:18:17 +0800

    New api, new cdn for css and js.

--- server/server_v1.py
+++ server/server_v1.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 BRAND = 'CodingHonor'
-DOMAIN = 'localhost'
+DOMAIN = 'localhost:5000'
 HOST = '0.0.0.0'
 PORT = 5000
 
@@ -43,23 +43,21 @@ def album(albumId=''):
 	soup = BeautifulSoup(requests.get(url, headers=HEADERS).text)
 	data = {}
 	data['id'] = albumId
-	data['name'] = soup.find('h2', class_='f-ff2').get_text()
+	data['name'] = getText(soup.find('h2', class_='f-ff2'))
 	data['artist'] = {}
-	data['artist']['name'] = soup.find('a', class_='s-fc7').get_text()
-	last = lambda s, sp: re.split(sp, s if s is not None else '')[-1].strip()
-	data['artist']['id'] = last(soup.find('a', class_='s-fc7')['href'], 'id=')
-	intrs = soup.find_all('p', class_='intr')
-	if (len(intrs) >= 3):
-		data['publishTime'] = last(intrs[1].get_text(), '：[ \n]*')
-		data['company'] = last(intrs[2].get_text(), '：[ \n]*')
+	data['artist']['name'] = getText(soup.find('a', class_='s-fc7'))
+	data['artist']['id'] = last('id=', soup.find('a', class_='s-fc7')['href'])
+	intrNodes = soup.find_all('p', class_='intr')
+	if len(intrNodes) >= 3:
+		data['publishTime'] = last('：[ \n]*', getText(intrNodes[1]))
+		data['company'] = last('：[ \n]*', getText(intrNodes[2]))
 	data['songs'] = []
-	songs = soup.find_all('tr', class_='ztag')
-	for song in songs:
-		d = {}
-		d['id'] = song['data-id']
-		d['name'] = song.a['title']
-		d['time'] = song.find('td', 's-fc3').get_text()
-		data['songs'].append(d)
+	for songNode in soup.find_all('tr', class_='ztag'):
+		songDict = {}
+		songDict['id'] = songNode['data-id']
+		songDict['name'] = songNode.a['title']
+		songDict['time'] = getText(songNode.find('td', 's-fc3'))
+		data['songs'].append(songDict)
 	return jsonify(**data)
 
 
@@ -67,8 +65,40 @@ def album(albumId=''):
 def albums():
 	s, = parse_request('s')
 	offset, limit = parse_offset_limit();
-	url = 'http://music.163.com/api/search/get'
+	url = 'http://music.163.com/api/search/get/web?csrf_token='
 	payload = {'s': s, 'type': '10', 'offset': offset, 'total': 'true', 'limit': limit}
+	r = requests.post(url, data=payload, headers=HEADERS)
+	return jsonify(**r.json())
+
+
+@app.route('/v1/artists/<artistId>')
+def artist(artistId=''):
+	url = 'http://music.163.com/artist?id=%s' % artistId
+	soup = BeautifulSoup(requests.get(url, headers=HEADERS).text)
+	data = {}
+	data['id'] = artistId
+	data['name'] = getText(soup.find('h2', id='artist-name'))
+	data['songs'] = []
+	for songNode in soup.find_all('tr', class_='ztag'):
+		songDict = {}
+		idNameNode = songNode.find('div', class_='ttc').a
+		songDict['id'] = last('id=', idNameNode['href'] if idNameNode is not None else '')
+		songDict['name'] = getText(idNameNode)
+		songDict['time'] = getText(songNode.find('td', 's-fc3'))
+		albumNode = songNode.find('td', class_='w4').a
+		songDict['album'] = {}
+		songDict['album']['id'] = last('id=', albumNode['href'] if albumNode is not None else '')
+		songDict['album']['name'] = getText(albumNode)
+		data['songs'].append(songDict)
+	return jsonify(**data)
+
+
+@app.route('/v1/artists')
+def artists():
+	s, = parse_request('s')
+	offset, limit = parse_offset_limit();
+	url = 'http://music.163.com/api/search/get/web?csrf_token='
+	payload = {'s': s, 'type': '100', 'offset': offset, 'total': 'true', 'limit': limit}
 	r = requests.post(url, data=payload, headers=HEADERS)
 	return jsonify(**r.json())
 
@@ -78,6 +108,44 @@ def newAlbums(area='ALL'):
 	offset, limit = parse_offset_limit();
 	url = 'http://music.163.com/api/album/new?area=%s&offset=%d&total=true&limit=%d&csrf_token=' % (area, offset, limit)
 	r = requests.get(url, headers=HEADERS)
+	return jsonify(**r.json())
+
+
+@app.route('/v1/playlists/<playlistId>')
+def playlist(playlistId=''):
+	url = 'http://music.163.com/playlist?id=%s' % playlistId
+	soup = BeautifulSoup(requests.get(url, headers=HEADERS).text)
+	data = {}
+	data['id'] = playlistId
+	data['name'] = getText(soup.find('h2', class_='f-ff2'))
+	data['songs'] = []
+	for songNode in soup.find_all('tr', class_='ztag'):
+		songDict = {}
+		tds = songNode.find_all('td')
+		if len(tds) >= 5:
+			idNameNode = tds[1].find('div', class_='ttc').a
+			songDict['id'] = last('id=', idNameNode['href'] if idNameNode is not None else '')
+			songDict['name'] = getText(idNameNode)
+			songDict['time'] = getText(tds[2])
+			artistNode = tds[3].a
+			songDict['artist'] = {}
+			songDict['artist']['id'] = last('id=', artistNode['href'] if artistNode is not None else '')
+			songDict['artist']['name'] = getText(tds[3])
+			albumaNode = tds[4].a
+			songDict['album'] = {}
+			songDict['album']['id'] = last('id=', albumaNode['href'] if albumaNode is not None else '')
+			songDict['album']['name'] = getText(albumaNode)
+			data['songs'].append(songDict)
+	return jsonify(**data)
+
+
+@app.route('/v1/playlists')
+def playlists():
+	s, = parse_request('s')
+	offset, limit = parse_offset_limit();
+	url = 'http://music.163.com/api/search/get/web?csrf_token='
+	payload = {'s': s, 'type': '1000', 'offset': offset, 'total': 'true', 'limit': limit}
+	r = requests.post(url, data=payload, headers=HEADERS)
 	return jsonify(**r.json())
 
 
@@ -100,6 +168,14 @@ def parse_offset_limit(*params):
 	except ValueError:
 		ret[1] = LIMIT
 	return ret
+
+
+def getText(node):
+	return node.get_text() if node is not None else ''
+
+
+def last(sp, s):
+	return re.split(sp, s if s is not None else '')[-1].strip()
 
 
 if __name__ == '__main__':
